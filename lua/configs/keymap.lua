adc4ea738e1f02345e94c2163d2ed3f06efd7d92
ikhlashmulya <nurahmanmulya@gmail.com>
2025-08-10 11:04:05 +0700

    update keymap

--- lua/configs/keymap.lua
+++ lua/configs/keymap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>f", vim.cmd.Oil)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>/", vim.cmd.nohlsearch)
 
 vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>")
 vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>")
